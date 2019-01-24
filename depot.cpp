7979beb53e2a499163202ae44c4d5383433ca527
MaxiPineker <maxi.pineker@gmail.com>
2019-01-24 13:27:55 +0100

    it builds

---
+++ depot.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "depot.h"
+
+Depot create_depot()
+{
+  return 0;
+}
+void delete_depot(Depot depot)
+{
+
+}
+void push_depot(Depot depot, Product *product)
+{
+
+}
+int get_count(Depot depot)
+{
+  return 0;
+}
+Product* pop_depot(Depot depot)
+{
+  return 0;
+}
---
+++ stack.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "stack.h"
+
+
+Stack create_stack()
+{
+  return 0;
+}
+void delete_stack(Stack stack)
+{
+
+}
+void push_stack(Stack stack, void *data)
+{
+
+}
+int get_count(Stack stack)
+{
+
+}
+void* pop_stack(Stack stack)
+{
+
+}
+void* peek_stack(Stack stack)
+{
+
+}
