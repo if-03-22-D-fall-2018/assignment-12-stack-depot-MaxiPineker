3662864be4db15507ef258f20cf4dbb97c6925b2
MaxiPineker <maxi.pineker@gmail.com>
2019-01-24 13:34:21 +0100

    stack.cpp began

--- stack.cpp
+++ stack.cpp
@@ -1,10 +1,21 @@
 #include <stdlib.h>
 #include "stack.h"
 
+struct Node {
+    struct Node* next;
+    void* data;
+};
+
+struct StackImplementation {
+    struct Node* head;
+    int count;
+};
 
 Stack create_stack()
 {
-  return 0;
+  Stack stack = (Stack)smalloc(sizeof(StackImplementation));
+  stack->head = 0;
+  stack->count = 0;
 }
 void delete_stack(Stack stack)
 {
@@ -16,7 +27,7 @@ void push_stack(Stack stack, void *data)
 }
 int get_count(Stack stack)
 {
-
+  return stack->count;;
 }
 void* pop_stack(Stack stack)
 {
